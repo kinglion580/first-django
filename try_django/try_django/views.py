21db7f8e663b927458fa2e884a1e2cd018a609e0
kinglion <1609019405@qq.com>
2019-08-19 19:01:06 +0800

    built-in template tag

--- try_django/try_django/views.py
+++ try_django/try_django/views.py
@@ -1,12 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template.loader import get_template
 
 
 def home_page(request):
     my_title = "Hello there...."
     # doc = "<h1>{title}</h1>".format(title=title)
     # django_rendered_doc = "<h1>{{title}}</h1>".format(title=title)
-    return render(request, "hello_world.html", {"title": my_title})
+    context = {"title": "my title"}
+    if request.user.is_authenticated:
+        context = {"title": my_title, "my_list": [1,2,3,4,5]}
+    return render(request, "home.html", context)
 
 
 def about_page(request):
@@ -15,3 +19,10 @@ def about_page(request):
 
 def contact_page(request):
     return render(request, "hello_world.html", {"title": "Contact us"})
+
+
+def example_page(request):
+    context = {"title": "Example"}
+    template_name = "hello_world.html"
+    template_obj = get_template(template_name)
+    return HttpResponse(template_obj.render(context))
